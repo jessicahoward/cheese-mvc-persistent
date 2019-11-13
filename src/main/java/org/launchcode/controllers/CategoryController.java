f55c155bbca1f36445dd875bfda18f211975b3eb
jessicahoward <jessicahoward1031@gmail.com>
2019-11-13 11:12:07 -0500

    add category display functionality

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -4,6 +4,7 @@ package org.launchcode.controllers;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +13,13 @@ public class CategoryController {
 
     @Autowired
     private CategoryDao categoryDao;
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "Categories");
+
+        return "category/index";
+    }
 }
